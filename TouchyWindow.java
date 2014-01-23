99b21402c4a66726a3941e06f331b4f72087a81d
Andiry Xu <jix024@cs.ucsd.edu>
2014-01-23 11:29:24 -0800

    FunnyFace

--- TouchyWindow.java
+++ TouchyWindow.java
@@ -3,12 +3,35 @@ import java.awt.*;
 
 public class TouchyWindow extends WindowController
 {
-	public void onMouseMove(Location point)
-	{
-		new Text("Touched", point.getX(), point.getY(), canvas);
+	private static final int FL = 170, FR = 170;
+	private FunnyFace happy;
+	private Location lastpoint;
+	private boolean grabbed = false;
+
+	public void begin() {
+		happy = new FunnyFace(FL, FR, canvas);
 	}
+
+	public void onMousePress(Location point)
+	{
+//		new Text("Touched", point.getX(), point.getY(), canvas);
+		lastpoint = point;
+		grabbed = happy.contains(point);
+	}
+
+	public void onMouseDrag(Location point)
+	{
+//		new Text("Touched", point.getX(), point.getY(), canvas);
+		if (grabbed) {
+			happy.move(point.getX() - lastpoint.getX(),
+					point.getY() - lastpoint.getY());
+		}
+		lastpoint = point;
+	}
+
 	public void onMouseRelease(Location point)
 	{
-		canvas.clear();
+//		canvas.clear();
+//		grabbed = false;
 	}
 }
